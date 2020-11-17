eb25bacca34c80534fbf81fb6d0c8358c2efc707
IcaarusDev <icarusbeherit@protonmail.com>
2020-11-17 14:43:25 -0300

    building recyclerview

--- app/src/main/java/com/icaarusdev/ezymoviefinder/view/MovieDetailFragment.kt
+++ app/src/main/java/com/icaarusdev/ezymoviefinder/view/MovieDetailFragment.kt
@@ -6,14 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.icaarusdev.ezymoviefinder.R
+import kotlinx.android.synthetic.main.fragment_movie_detail.*
 
 
 class MovieDetailFragment : Fragment() {
+
+    private var movieId = 0;
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let{
+            movieId = MovieDetailFragmentArgs.fromBundle(it).movieId
+        }
+
     }
 }
--- app/src/main/java/com/icaarusdev/ezymoviefinder/view/MovieListFragment.kt
+++ app/src/main/java/com/icaarusdev/ezymoviefinder/view/MovieListFragment.kt
@@ -5,16 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.icaarusdev.ezymoviefinder.R
-
+import com.icaarusdev.ezymoviefinder.view.MovieDetailFragmentDirections.actionMovieListFragment
+import kotlinx.android.synthetic.main.fragment_movie_list.*
 
 class MovieListFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 
 }
