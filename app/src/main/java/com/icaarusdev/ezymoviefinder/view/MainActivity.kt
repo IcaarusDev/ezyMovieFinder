8c27d1cf76adc66add5737c2b9fef1417893def0
IcaarusDev <icarusbeherit@protonmail.com>
2020-11-16 22:52:19 -0300

    adding dependencies

--- app/src/main/java/com/icaarusdev/ezymoviefinder/view/MainActivity.kt
+++ app/src/main/java/com/icaarusdev/ezymoviefinder/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.icaarusdev.ezymoviefinder
+package com.icaarusdev.ezymoviefinder.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.icaarusdev.ezymoviefinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
