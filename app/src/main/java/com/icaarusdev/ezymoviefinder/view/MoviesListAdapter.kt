10c9213e0951ff90cf36ad68d730055f4e28a1c8
IcaarusDev <icarusbeherit@protonmail.com>
2020-11-21 09:05:45 -0300

    using databinding

--- app/src/main/java/com/icaarusdev/ezymoviefinder/view/MoviesListAdapter.kt
+++ app/src/main/java/com/icaarusdev/ezymoviefinder/view/MoviesListAdapter.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.icaarusdev.ezymoviefinder.R
+import com.icaarusdev.ezymoviefinder.databinding.ItemMovieBinding
 import com.icaarusdev.ezymoviefinder.model.Movie
 import com.icaarusdev.ezymoviefinder.util.getImageProgress
 import com.icaarusdev.ezymoviefinder.util.setImage
@@ -30,34 +32,36 @@ class MoviesListAdapter(val moviesList: ArrayList<Movie>) :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
         val inflater = LayoutInflater.from(parent.context)
 
-        listViewModel = ViewModelProviders.of((parent.context as FragmentActivity)).get(
-            ListViewModel::class.java
-        )
-        val view = inflater.inflate(R.layout.item_movie, parent, false)
+//        listViewModel = ViewModelProviders.of((parent.context as FragmentActivity)).get(
+//            ListViewModel::class.java
+//        )
+//        val view = inflater.inflate(R.layout.item_movie, parent, false)
+        val view = DataBindingUtil.inflate<ItemMovieBinding>(inflater, R.layout.item_movie, parent,false)
         return MovieViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
-        holder.view.txMovieTitle.text = moviesList[position].title
-        holder.view.txMovieDescription.text = moviesList[position].overview
-        holder.view.setOnClickListener {
-            //Toast.makeText(holder.view.context,"${moviesList[position].id}",Toast.LENGTH_SHORT).show()
-            //Navigation.findNavController(it).navigate(MovieListFragmentDirections.actionMovieDetailFragment())
-            val action = MovieListFragmentDirections.actionMovieDetailFragment()
-            moviesList[position].id?.toInt()?.let { it1 -> action.setId(it1) }
-            holder.view.findNavController().navigate(action)
-        }
-
-        holder.view.favoriteIcon.setOnClickListener {
-            moviesList[position].id?.toInt()?.let { it1 -> listViewModel.updateFavorite(it1) }
-        }
-
-        moviesList[position].backDropPath?.let {
-            holder.view.movieImage.setImage(it, getImageProgress(holder.view.movieImage.context))
-        }
+        holder.view.movie = moviesList[position]
+//        holder.view.txMovieTitle.text = moviesList[position].title
+//        holder.view.txMovieDescription.text = moviesList[position].overview
+//        holder.view.setOnClickListener {
+//            //Toast.makeText(holder.view.context,"${moviesList[position].id}",Toast.LENGTH_SHORT).show()
+//            //Navigation.findNavController(it).navigate(MovieListFragmentDirections.actionMovieDetailFragment())
+//            val action = MovieListFragmentDirections.actionMovieDetailFragment()
+//            moviesList[position].id?.toInt()?.let { it1 -> action.setId(it1) }
+//            holder.view.findNavController().navigate(action)
+//        }
+//
+//        holder.view.favoriteIcon.setOnClickListener {
+//            moviesList[position].id?.toInt()?.let { it1 -> listViewModel.updateFavorite(it1) }
+//        }
+//
+//        moviesList[position].backDropPath?.let {
+//            holder.view.movieImage.setImage(it, getImageProgress(holder.view.movieImage.context))
+//        }
     }
 
     override fun getItemCount() = moviesList.size
 
-    class MovieViewHolder(var view: View) : RecyclerView.ViewHolder(view)
+    class MovieViewHolder(var view: ItemMovieBinding) : RecyclerView.ViewHolder(view.root)
 }
